dd30621c898d44891275f09baf57dfc588f08886
Nguyen Bui Tiep <buitiep379@gmail.com>
2022-03-05 13:05:59 +0700

    add file

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  plugins: ['prettier'],
+}
